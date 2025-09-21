d7fa988654244180ae1dd321dcb521df781c6506
Shah Arif <55637747+shah-arif@users.noreply.github.com>
2025-09-22 00:55:28 +0600

    end of section 7

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -5,12 +5,25 @@ import { MemberDetail } from './members/member-detail/member-detail';
 import { Lists } from './lists/lists';
 import { Messages } from './messages/messages';
 import { authGuard } from './_guards/auth-guard';
+import { TestErrors } from './errors/test-errors/test-errors';
+import { NotFound } from './errors/not-found/not-found';
+import { ServerError } from './errors/server-error/server-error';
 
 export const routes: Routes = [
     {path: '', component: Home},
-    {path: 'members', component: MemberList, canActivate: [authGuard]},
-    {path: 'members/:id', component: MemberDetail},
-    {path: 'lists', component: Lists},
-    {path: 'messages', component: Messages},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'members', component: MemberList, canActivate: [authGuard]},
+            {path: 'members/:id', component: MemberDetail},
+            {path: 'lists', component: Lists},
+            {path: 'messages', component: Messages},
+        ]
+    },
+    {path: 'errors', component: TestErrors},
+    {path: 'not-found', component: NotFound},
+    {path: 'server-error', component: ServerError},
     {path: '**', component: Home, pathMatch: 'full'},
 ];
